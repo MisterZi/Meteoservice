c24dfe29f51e81c2eedcd5697f16505e204815d0
ale8270 <ale8270@yandex.ru>
2016-05-17 20:39:27 +0300

    v1.1 add future weather

--- pogoda.rb
+++ pogoda.rb
@@ -12,24 +12,27 @@ response = Net::HTTP.get_response(uri)
 
 doc = REXML::Document.new(response.body)
 
-city_name = doc.root.elements['REPORT/TOWN'].attributes['index']
-date_str = doc.root.elements['REPORT/TOWN/FORECAST'].attributes['day'] + '.' +
-    doc.root.elements['REPORT/TOWN/FORECAST'].attributes['month'] + '.' +
-    doc.root.elements['REPORT/TOWN/FORECAST'].attributes['year']
-date = Date.parse(date_str)
-time = doc.root.elements['REPORT/TOWN/FORECAST'].attributes['hour']
-temperature = doc.root.elements['REPORT/TOWN/FORECAST/TEMPERATURE'].attributes['max']
-wind = doc.root.elements['REPORT/TOWN/FORECAST/WIND'].attributes['max']
-phenomena = doc.root.elements['REPORT/TOWN/FORECAST/PHENOMENA'].attributes['cloudiness'].to_i
-cloudiness = ''
+city_name = doc.root.elements['REPORT/TOWN'].attributes['sname']
 
-case phenomena
-  when 0 then cloudiness = 'ясно'
-  when 1 then cloudiness = 'малооблачно'
-  when 2 then cloudiness = 'облачно'
-  when 3 then cloudiness = 'пасмурно'
+doc.root.elements.each('REPORT/TOWN/FORECAST') do |forecast|
+  date_str = forecast.attributes['day'] + '.' +
+             forecast.attributes['month'] + '.' +
+             forecast.attributes['year']
+  date = Date.parse(date_str)
+  time = forecast.attributes['hour']
+  temperature = forecast.elements['TEMPERATURE'].attributes['max']
+  wind = forecast.elements['WIND'].attributes['max']
+  phenomena = forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
+  cloudiness = ''
+
+  case phenomena
+    when 0 then cloudiness = 'ясно'
+    when 1 then cloudiness = 'малооблачно'
+    when 2 then cloudiness = 'облачно'
+    when 3 then cloudiness = 'пасмурно'
+  end
+
+  puts
+  puts "Погода в городе #{city_name} на #{time} час #{date.strftime('%d-%m-%Y')}:"
+  puts "#{temperature} градусов, #{cloudiness}, ветер #{wind} м/с"
 end
-
-
- puts "Погода в городе #{city_name} на #{time} час #{date.strftime('%d-%m-%Y')}:"
-puts "#{temperature} градусов, #{cloudiness}, ветер #{wind} м/с"
