ddb61fb2f729c86bcc972e94dc87e0381b77db66
ale8270 <ale8270@yandex.ru>
2016-08-24 20:36:25 +0300

    release

--- meteoservice.rb
+++ meteoservice.rb
@@ -6,29 +6,47 @@ require 'date'
 
 class Meteoservice
 
-  def self.get_city_id(xml_file, city_name)
-    doc = REXML::Document.new(xml_file)
-
-    city_id = 69
-
-    doc.root.elements.each do |city|
-      city_id = city.attributes['value'] if city_name == city.text
-    end
-    return city_id
+  def initialize(city_name)
+    @city_name = city_name
   end
 
+  # Получает id введенного города из списка city_list.xml
+  def get_city_id
+    current_path = File.dirname(__FILE__)
+    file_name = current_path + '/city_list.xml'
+    file = File.new(file_name, 'r:utf-8')
+    doc = REXML::Document.new(file)
+    file.close
 
-  def take_data(city_id)
-    @uri = URI.parse("http://xml.meteoservice.ru/export/gismeteo/point/#{city_id}.xml")
-    response = Net::HTTP.get_response(@uri)
-    # исключения!!
+    city_id = nil
+    doc.root.elements.each do |city|
+      city_id = city.attributes['value'] if @city_name == city.text
+    end
+
+    # Если такого города нет - завершаем программу
+    if city_id.nil?
+      abort('Город не найден!')
+    else
+      @meteodata = {city_id: "#{city_id}"}
+    end
+  end
+
+  # Делает запрос на метеосервис (meteoservice.ru) и получает с него xml с данными о погоде
+  def take_data
+    uri = URI.parse("http://xml.meteoservice.ru/export/gismeteo/point/#{@meteodata[:city_id]}.xml")
+    # Исключения на различные ошибки Net::HTTP
+    begin
+      response = Net::HTTP.get_response(uri)
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+           Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => error
+      abort "Ошибка! #{error}"
+    end
+
     doc = REXML::Document.new(response.body)
 
-
-    @meteodata = {city_name: "#{URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])}"}
+    @meteodata[:city_name] = URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
 
     data = []
-
     doc.root.elements.each('REPORT/TOWN/FORECAST') do |forecast|
       another_data = {date: Date.parse(
               forecast.attributes['day'] + '.' +
@@ -48,13 +66,15 @@ class Meteoservice
         when 3 then
           another_data[:cloudiness] = 'пасмурно'
       end
-
       data << another_data
     end
     @meteodata[:data] = data
   end
 
   def show_weather
+    get_city_id
+    take_data
+
     puts "\nПогода в городе #{@meteodata[:city_name]}: "
 
     @meteodata[:data].each do |data|
--- pogoda.rb
+++ pogoda.rb
@@ -1,23 +1,8 @@
 
 require_relative 'meteoservice.rb'
 
-current_path = File.dirname(__FILE__)
-file_name = current_path + '/city_list.xml'
-
-
 puts 'Введите название города:'
 city_name = STDIN.gets.chomp
 
-file = File.new(file_name, 'r:utf-8')
-city_id = Meteoservice.get_city_id(file, city_name)
-file.close
-
-
-meteoservice = Meteoservice.new
-meteoservice.take_data(city_id)
+meteoservice = Meteoservice.new(city_name)
 meteoservice.show_weather
-
-
-
-
-
